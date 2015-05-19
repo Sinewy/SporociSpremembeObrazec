8df13d99135a5a34afd0d5174fa10204bfcab4c9
Sinewy <sinewy@gmx.com>
2015-05-19 15:52:31 +0200

    First draft of the form.

--- js/mainScript.js
+++ js/mainScript.js
@@ -1,3 +1,76 @@
 /**
  * Created by jurez on 05/15/2015.
  */
+$(document).ready(function() {
+
+    $("#personPersonalBtn").click(function() {
+        if($("#personPersonalBtn").hasClass("notSelected")) {
+            $("#personPersonalBtn").removeClass("notSelected");
+            if(!$("#personBusinessBtn").hasClass("notSelected")) {
+                $("#personBusinessBtn").addClass("notSelected");
+            }
+            $("#personType").val("fizicna");
+            if($(".selectActionType").hasClass("hidden")) {
+                $(".selectActionType").fadeIn();
+                $(".selectActionType").removeClass("hidden");
+            }
+        }
+    });
+
+    $("#personBusinessBtn").click(function() {
+        if($("#personBusinessBtn").hasClass("notSelected")) {
+            $("#personBusinessBtn").removeClass("notSelected");
+            if(!$("#personPersonalBtn").hasClass("notSelected")) {
+                $("#personPersonalBtn").addClass("notSelected");
+            }
+            $("#personType").val("pravna");
+            if($(".selectActionType").hasClass("hidden")) {
+                $(".selectActionType").fadeIn();
+                $(".selectActionType").removeClass("hidden");
+            }
+        }
+    });
+
+    $("#newDataBtn").click(function() {
+        if($("#newDataBtn").hasClass("notSelected")) {
+            $("#newDataBtn").removeClass("notSelected");
+            if(!$("#updateDataBtn").hasClass("notSelected")) {
+                $("#updateDataBtn").addClass("notSelected");
+            }
+            $("#actionType").val("vpis");
+            if($(".formNewData").hasClass("hidden")) {
+                $(".formNewData").fadeIn();
+                $(".formNewData").removeClass("hidden");
+            }
+            if(!$(".formUpdateData").hasClass("hidden")) {
+                $(".formUpdateData").fadeOut();
+                $(".formUpdateData").addClass("hidden");
+            }
+        }
+    });
+
+    $("#updateDataBtn").click(function() {
+        if($("#updateDataBtn").hasClass("notSelected")) {
+            $("#updateDataBtn").removeClass("notSelected");
+            if(!$("#newDataBtn").hasClass("notSelected")) {
+                $("#newDataBtn").addClass("notSelected");
+            }
+            $("#actionType").val("sprememba");
+            if($(".formUpdateData").hasClass("hidden")) {
+                $(".formUpdateData").fadeIn();
+                $(".formUpdateData").removeClass("hidden");
+            }
+            if(!$(".formNewData").hasClass("hidden")) {
+                $(".formNewData").fadeOut();
+                $(".formNewData").addClass("hidden");
+            }
+        }
+    });
+
+    $("#moreInfoPublishing").hover(function() {
+
+    });
+
+
+
+});
